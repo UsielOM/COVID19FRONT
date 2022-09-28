ebdc875ce958b54326ec3d6f59ac70d1a33db6ce
UsielOM <usiel31197@outlook.es>
2022-09-27 20:51:37 -0500

    Metodo registrar

--- src/app/Pages/home/Components/register/register.component.ts
+++ src/app/Pages/home/Components/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Register } from 'src/app/Interface/register';
+import { PostService } from 'src/app/Service/post.service';
 
 @Component({
   selector: 'app-register',
@@ -7,8 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  reg: Register = new Register();
 
+  constructor(private postService:PostService) { }
+
+
+  registroUser(): void{
+    this.postService.createRegistroUser(this.reg).subscribe(result =>{
+      this.reg = result;
+    });
+  }
   ngOnInit(): void {
   }
 
