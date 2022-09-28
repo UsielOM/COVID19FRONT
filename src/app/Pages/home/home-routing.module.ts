56bba56cbf87ea87397ae206f0c69bf677c32730
UsielOM <usiel31197@outlook.es>
2022-09-27 20:21:34 -0500

    Estilo registro componente y rutas

--- src/app/Pages/home/home-routing.module.ts
+++ src/app/Pages/home/home-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './Components/login/login.component';
+import { RegisterComponent } from './Components/register/register.component';
 import { HomePagesComponent } from './home-pages.component';
 
 const routes: Routes = [
   {path: '', component:HomePagesComponent, 
   children: [
-    {path:'', component:LoginComponent}
+    {path:'', component:LoginComponent},
+    {path: 'registro', component:RegisterComponent}
   ]
 }
 
