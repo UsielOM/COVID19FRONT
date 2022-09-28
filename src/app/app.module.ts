ed9290eb08958d3e4a540d150bbbccfc78637d32
UsielOM <usiel31197@outlook.es>
2022-09-27 19:54:01 -0500

    Craecion de modulos de Home y User

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeModuleModule } from './Pages/home-module/home-module.module';
+import { UserModuleModule } from './Pages/user-module/user-module.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HomeModuleModule,
+    UserModuleModule,
+    AppRoutingModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
