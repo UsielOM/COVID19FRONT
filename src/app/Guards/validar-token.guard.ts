bf1fb74fdb2a11ab8a4df8030877c8af2043cd86
UsielOM <usiel31197@outlook.es>
2022-09-27 21:53:52 -0500

    Guard ruta user

---
+++ src/app/Guards/validar-token.guard.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import {CanActivate, CanLoad, Router} from '@angular/router';
+import { Observable } from 'rxjs';
+
+import {of, tap} from 'rxjs' ;
+import { AuthService } from '../Service/auth.service';
+
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ValidarTokenGuard implements CanActivate, CanLoad {
+
+  constructor(private authService : AuthService, private router : Router) {
+  }
+
+  canActivate(): Observable<boolean> | boolean {
+    return this.authService.validarToken()
+    .pipe(
+        tap(valid =>{
+            if(!valid){
+                this.router.navigateByUrl('')
+            }
+        })
+    );
+  }
+
+  canLoad(): Observable<boolean> | boolean {
+    return this.authService.validarToken()
+    .pipe(
+        tap(valid =>{
+            if(!valid){
+                this.router.navigateByUrl('')
+            }
+        })
+    );
+  }
+
+}
