47eae1cd409b0898e2e34b88ba15acc66151f185
UsielOM <usiel31197@outlook.es>
2022-09-27 20:46:53 -0500

    Servicio post y inter

--- src/app/Service/post.service.ts
+++ src/app/Service/post.service.ts
@@ -1,9 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Register } from '../Interface/register';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PostService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  createRegistroUser(register:Register):Observable<Register>{
+    return this.http.post<Register>(environment.apiUrl + "/post/userRegistro", register )
+  }
 }
