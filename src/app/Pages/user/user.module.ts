a99bfe19c395c3a842d94d1e87c668cef38959ab
UsielOM <usiel31197@outlook.es>
2022-09-27 21:01:05 -0500

    Componentes page user

--- src/app/Pages/user/user.module.ts
+++ src/app/Pages/user/user.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { UserRoutingModule } from './user-routing.module';
+import { UserPagesComponent } from './user-pages.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    UserPagesComponent
+  ],
   imports: [
     CommonModule,
     UserRoutingModule
