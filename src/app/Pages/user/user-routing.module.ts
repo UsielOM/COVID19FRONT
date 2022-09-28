78a7b8abe3850c04b37e92b8968e5af63bbbb497
UsielOM <usiel31197@outlook.es>
2022-09-27 21:04:13 -0500

    Ruats user

--- src/app/Pages/user/user-routing.module.ts
+++ src/app/Pages/user/user-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UserPagesComponent } from './user-pages.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path: 'User', component:UserPagesComponent, 
+children:[
+  
+]
+}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,10 @@ const routes: Routes = [
 
   {path:'',
 loadChildren:() => import('./Pages/home/home.module').then(m => m.HomeModule)
+},
+{path: 'User',
+loadChildren:() => import('./Pages/user/user.module').then(m =>m.UserModule)
+
 }
 ];
 
