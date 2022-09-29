69fd52ac5b8ab50aefd25c665e19003f3491c754
UsielOM <usiel31197@outlook.es>
2022-09-28 21:25:44 -0500

    Ruta y estilos

--- src/app/Pages/user/user-routing.module.ts
+++ src/app/Pages/user/user-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { NavbarUserComponent } from './Components/navbar-user/navbar-user.component';
 import { SidbarUserComponent } from './Components/sidbar-user/sidbar-user.component';
 import { UserPagesComponent } from './user-pages.component';
+import { UserPerfilComponent } from './Components/user-perfil/user-perfil.component';
 
 const routes: Routes = [
 
   {path: '', component:UserPagesComponent, 
 children:[
-{path: '', component:SidbarUserComponent}
+{path: '', component:SidbarUserComponent},
+{path: 'perfil', component:UserPerfilComponent}
 ]
 }
 ];
