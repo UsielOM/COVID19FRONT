03ae4720d6341a08fbb18445ee083acb83db3a50
UsielOM <usiel31197@outlook.es>
2022-09-28 21:03:27 -0500

    Creacion de componente navbar

--- src/app/Pages/user/user.module.ts
+++ src/app/Pages/user/user.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { UserRoutingModule } from './user-routing.module';
 import { UserPagesComponent } from './user-pages.component';
+import { NavbarUserComponent } from './Components/navbar-user/navbar-user.component';
 
 
 @NgModule({
   declarations: [
-    UserPagesComponent
+    UserPagesComponent,
+    NavbarUserComponent
   ],
   imports: [
     CommonModule,
