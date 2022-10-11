0635e0b33ae7c7d46aa910d24fe027fd53a6ee8a
UsielOM <usiel31197@outlook.es>
2022-10-11 14:58:08 -0500

    Ruta encuesta

--- src/app/Pages/user/user-routing.module.ts
+++ src/app/Pages/user/user-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CuestionarioComponent } from './Components/cuestionario/cuestionario.component';
 import { NavbarUserComponent } from './Components/navbar-user/navbar-user.component';
 import { PerfilComponent } from './Components/perfil/perfil.component';
 import { SidbarUserComponent } from './Components/sidbar-user/sidbar-user.component';
@@ -11,7 +12,8 @@ const routes: Routes = [
   {path: '', component:UserPagesComponent, 
 children:[
 {path: '', component:SidbarUserComponent},
-{path:'perfil', component:PerfilComponent}
+{path:'perfil', component:PerfilComponent},
+{path:'encuesta', component:CuestionarioComponent},
 
 
 ]
