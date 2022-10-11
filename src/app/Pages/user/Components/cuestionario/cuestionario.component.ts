23ac41b3cc78e753e56444fa4927bc5b975b1adb
UsielOM <usiel31197@outlook.es>
2022-10-11 15:25:03 -0500

    Diseño para la encuesta

--- src/app/Pages/user/Components/cuestionario/cuestionario.component.ts
+++ src/app/Pages/user/Components/cuestionario/cuestionario.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cuestionario.component.css']
 })
 export class CuestionarioComponent implements OnInit {
-
+  preguntasBasicas:any [] = []; 
   constructor() { }
 
   ngOnInit(): void {
