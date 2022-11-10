5207772526ba616d78e22486095bae1c25be33bc
UsielOM <usiel31197@outlook.es>
2022-11-10 13:51:45 -0600

    Servicio y metodo onsubmit para cargar y guardar las imagenes en el servidor

--- src/app/Pages/user/Components/signos-vitales/signos-vitales.component.ts
+++ src/app/Pages/user/Components/signos-vitales/signos-vitales.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { PostService } from 'src/app/Service/post.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-signos-vitales',
@@ -9,11 +11,30 @@ export class SignosVitalesComponent implements OnInit {
   title='uploadFiles';
   image = '';
   imgURL='../../../../../assets/upload.png'
-  constructor() { }
+  constructor( private postService:PostService) { }
 
   ngOnInit(): void {
   }
   onSubmit(){
+    const formData = new FormData();
+    formData.append('file', this.image)
+    this.postService.postImage(formData).subscribe((res) =>(Swal.fire({
+
+      icon:'success',
+      title:'Imagen Cargada',
+      text: 'La iamgen se subio correctamente'
+
+    }).then((result)=>{
+      if(result){
+        location.reload();
+      }
+    })
+    ), (err) =>Swal.fire({
+      icon:'error',
+      title: 'Opps..',
+      text:'Parece que no se subio nada!!!'
+    })
+    );
     
   }
   selectImage(event:any){
