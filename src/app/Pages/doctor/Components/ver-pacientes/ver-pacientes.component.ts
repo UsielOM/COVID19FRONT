7015612093d1b01a27330b130f59ea4ae67a44d9
UsielOM <usiel31197@outlook.es>
2022-11-25 18:01:45 -0600

    Recuperacion de la api de los signos vitales registrados

--- src/app/Pages/doctor/Components/ver-pacientes/ver-pacientes.component.ts
+++ src/app/Pages/doctor/Components/ver-pacientes/ver-pacientes.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Signos } from 'src/app/Interface/signos';
+import { GetServiceService } from 'src/app/Service/get-service.service';
 
 @Component({
   selector: 'app-ver-pacientes',
@@ -6,10 +8,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ver-pacientes.component.css']
 })
 export class VerPacientesComponent implements OnInit {
-
-  constructor() { }
-
+  signosGet: Signos [] = [];
+  constructor(private getService:GetServiceService) { }
+  
   ngOnInit(): void {
+    this.getService.getConsultas().subscribe(res =>{
+      this.signosGet = res;
+      console.log(this.signosGet);
+    })
   }
 
 }
