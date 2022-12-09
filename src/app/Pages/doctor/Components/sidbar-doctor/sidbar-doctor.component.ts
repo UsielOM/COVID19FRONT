d5bcce439b29f0dcc915a7506ccf3a5ec3b03cfb
UsielOM <usiel31197@outlook.es>
2022-12-09 09:47:43 -0600

    Componete sidbar doctor

---
+++ src/app/Pages/doctor/Components/sidbar-doctor/sidbar-doctor.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sidbar-doctor',
+  templateUrl: './sidbar-doctor.component.html',
+  styleUrls: ['./sidbar-doctor.component.css']
+})
+export class SidbarDoctorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/Pages/doctor/doctor.module.ts
+++ src/app/Pages/doctor/doctor.module.ts
@@ -5,13 +5,15 @@ import { DoctorRoutingModule } from './doctor-routing.module';
 import { DoctorPagesComponent } from './doctor-pages.component';
 import { VerPacientesComponent } from './Components/ver-pacientes/ver-pacientes.component';
 import { CitasHoyComponent } from './Components/citas-hoy/citas-hoy.component';
+import { SidbarDoctorComponent } from './Components/sidbar-doctor/sidbar-doctor.component';
 
 
 @NgModule({
   declarations: [
     DoctorPagesComponent,
     VerPacientesComponent,
-    CitasHoyComponent
+    CitasHoyComponent,
+    SidbarDoctorComponent
   ],
   imports: [
     CommonModule,
