68ef6195808c1b5bd4deb84cdebaff1bd645e900
UsielOM <usiel31197@outlook.es>
2023-01-26 10:36:50 -0600

    Eliminacion de componente que no se usa y modificacion de archivos

--- src/app/Pages/doctor/doctor-routing.module.ts
+++ src/app/Pages/doctor/doctor-routing.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { VerPacientesComponent } from './Components/ver-pacientes/ver-pacientes.component';
 import { DoctorPagesComponent } from './doctor-pages.component';
+import { ContenedorComponent } from './Components/contenedor/contenedor.component';
 
 const routes: Routes = [
 
   {path: '', component:DoctorPagesComponent,children:[
-    
+    {path: '', component:ContenedorComponent}
   ]}
 ];
 
